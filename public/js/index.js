2e1813d620f76eb7f36524e2ac43e6bb88c80d9a
DominicWuest <dominic.wuest@hotmail.com>
2019-02-05 19:43:39 +0100

    Added basic search functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,23 @@
 const maxSuggestions = 5;
-const suggestions = getSuggestions();
+let suggestions;
+
+function start(sug) {
+  suggestions = sug;
+}
 
 function showSuggested() {
-  var div = document.getElementById("suggestions"); // Remove every list element currently inside the div
+  let div = document.getElementById("suggestions"); // Remove every list element currently inside the div
   while (div.firstChild) {
     div.removeChild(div.firstChild);
   }
-  var input = document.getElementById("searchBar").value.toLowerCase();
+  let input = document.getElementById("searchBar").value.toLowerCase();
   if (input === '') return; // Don't show any recommendations if input is empty
-  var i = 0; // Counter to see how many list element have been created
-  for (suggestion of suggestions) {
-    if (i === maxSuggestions) break;
-    if (suggestion.toLowerCase().startsWith(input)) {
-      var node = document.createElement("LI");
-      var textNode = document.createTextNode(suggestion);
-      node.appendChild(textNode);
-      div.appendChild(node);
-      i++;
+  let suggestionsAmount = 0; // Counter to see how many list element have been created
+  for (let i = 0; i < suggestions[0].length; i++) {
+    for (keyword of suggestions[2][i]) {
+      if (keyword.startsWith(input)) {
+        console.log(suggestions[0][i]);
+      }
     }
   }
-}
-
-function getSuggestions() {
-  return ['A', 'Ab', 'Abc', 'abcd', 'abcde', 'abcdef'];
 }

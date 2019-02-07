e9ad0f4efc28cbb1c254b4328801fe9bd209b0a3
DominicWuest <dominic.wuest@hotmail.com>
2019-02-07 01:29:18 +0100

    Continued working

--- public/js/index.js
+++ public/js/index.js
@@ -48,3 +48,9 @@ function hideSuggested() { // Gets called when the searchbar loses focus
   }
   focused = false;
 }
+
+function checkDeviceSize() { // Alert the user if they are using a smaller device
+  if (screen.width < 1333 && screen.height < 1024) {
+    alert("Thank you for using our site! We noticed that you are accessing it with a smaller device. Please keep in mind that CSAcademy is best interacted with on a PC or Laptop.");
+  }
+}

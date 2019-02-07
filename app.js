83ba938fbc7388acec61a9cb101f6c2a6c48cadb
DominicWuest <dominic.wuest@hotmail.com>
2019-02-07 23:39:12 +0100

    Improved page for mobile devices

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // ----- Start of Imports -----
 
-var express = require("express");
-var bodyParser = require("body-parser");
-var fs = require("fs");
-var path = require("path");
-var csvParser = require("csv-parse/lib/sync");
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var csvParser = require('csv-parse/lib/sync');
 
 // ----- End of Imports -----
 
@@ -20,24 +20,24 @@ var courses = csvParser(fs.readFileSync('data/courses.csv'), {
 });
 
 courses[2] = courses[2].map(function (element) {
-  if (element.includes(" ")) return element.split(' '); // Return either the splitted string or the string in an array
+  if (element.includes(' ')) return element.split(' '); // Return either the splitted string or the string in an array
   else return [element];
 });
 
-app.use(express.static("public")); // Directory for static files like css
+app.use(express.static('public')); // Directory for static files like css
 app.set('views', path.join(__dirname, 'public/views')); // Static directory for ejs files
-app.set("view engine", "ejs"); // Set the view engine to ejs (res.render)
+app.set('view engine', 'ejs'); // Set the view engine to ejs (res.render)
 
 app.get('/', function(req, res) { // Homepage
-  res.render("index", {
-    "courses" : courses,
-    "mostPopular" : mostPopular
+  res.render('index', {
+    'courses' : courses,
+    'mostPopular' : mostPopular
   });
 });
 
 app.use(function(req, res, next) {
   res.status(404);
-  res.render("404");
+  res.render('404');
 });
 
 app.listen(3000); // Listen on port 3000

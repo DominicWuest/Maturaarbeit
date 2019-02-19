6599826d971cfd37b55e0ecde2d893a3c728564a
DominicWuest <dominic.wuest@hotmail.com>
2019-02-19 23:22:08 +0100

    Changed min size of device for alert to pop up

--- public/js/index.js
+++ public/js/index.js
@@ -45,7 +45,7 @@ function hideSuggested() { // Gets called when the searchbar loses focus
 }
 
 function checkDeviceSize() { // Alert the user if they are using a smaller device
-  if (screen.width < 1333 && screen.height < 1024) {
+  if ((screen.width <= 768 && screen.height <= 1024) || (screen.width <= 1024 && screen.height <= 768)) {
     alert("Thank you for using our site! We noticed that you are accessing it with a smaller device. Please keep in mind that CSAcademy is best interacted with on a PC or Laptop.");
   }
 }

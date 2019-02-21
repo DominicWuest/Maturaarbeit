176dbbd8c7138f90725c8e8b8d32e1acae7a30c2
DominicWuest <dominic.wuest@hotmail.com>
2019-02-21 21:42:22 +0100

    Removed searchbar from index and included partial instead

--- public/js/partials/searchbar.js
+++ public/js/partials/searchbar.js
@@ -43,9 +43,3 @@ function hideSuggested() { // Gets called when the searchbar loses focus
   }
   focused = false;
 }
-
-function checkDeviceSize() { // Alert the user if they are using a smaller device
-  if ((screen.width <= 768 && screen.height <= 1024) || (screen.width <= 1024 && screen.height <= 768)) {
-    alert("Thank you for using our site! We noticed that you are accessing it with a smaller device. Please keep in mind that CSAcademy is best interacted with on a PC or Laptop.");
-  }
-}

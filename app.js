d3193f727e1b987a5f926271946f429d0db74981
Dominic Wüst <42354311+DominicWuest@users.noreply.github.com>
2019-03-06 18:51:17 +0100

    Added newest courses part on homepage and simplified process of adding most popular courses (#12)

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ var courses = csvParser(fs.readFileSync('data/courses.csv'), {
   comment : '$'
 });
 
+var newestCourses = csvParser(fs.readFileSync('data/newestCourses.csv'), {
+  comment : '$'
+});
+
 var pathsToCourses = courses[1].map(function(path) {
   if (path.length - path.replace(/\//g, '').length != 1) return path.substring(1).replace(/\//g, '/'); // File isn't the file which the folder is named after
   else return path.substring(1) + '/' + path.substring(1); // File is the file which the folder is named after
@@ -39,7 +43,8 @@ app.set('view engine', 'ejs'); // Set the view engine to ejs (res.render)
 app.get('/', function(req, res) { // Homepage
   res.render('index', {
     'courses' : courses,
-    'mostPopular' : mostPopular
+    'mostPopular' : mostPopular,
+    'newestCourses' : newestCourses
   });
 });
 

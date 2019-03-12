62523f50e8be8fab83487b8cd9673ebf541b138f
DominicWuest <dominic.wuest@hotmail.com>
2019-03-12 21:02:23 +0100

    Finished animation

--- public/js/algorithms/search/binary.js
+++ public/js/algorithms/search/binary.js
@@ -40,14 +40,80 @@ function addHighlighting() {
   }
 }
 
+let looped, value, elementsLength, max, min;
+let functions = [findTarget, setBounds], functionsIndex;
+let focus;
+let frames, fps = 1;
+let playing = false;
+
+function restartAnimation() {
+  setup();
+  looped = false;
+  let range = document.getElementById('range');
+  value = parseInt(range.value);
+  elementsLength = range.max - range.min;
+  max = elementsLength;
+  min = 0;
+  focus = elementsLength / 2;
+  range.style.padding = "0" + width / (elementsLength) / 2.5 + "px";
+  functionsIndex = 0;
+  frames = 0;
+}
+
 function setup() {
-  var canvasDiv = document.getElementById('animation');
-  var width = canvasDiv.offsetWidth - 50;
-  var height = canvasDiv.offsetHeight - 50;
-  var canvas = createCanvas(width, height);
+  let canvasDiv = document.getElementById('animation');
+  let width = canvasDiv.offsetWidth;
+  let height = canvasDiv.offsetHeight;
+  let canvas = createCanvas(width, height);
   canvas.parent('animation');
 }
 
 function draw() {
-  background(0, 0, 255);
+  clear();
+  for (let i = 0; i <= elementsLength; i++) {
+    if (i === focus && i === value) drawFocusValue();
+    else {
+      fill(0, 255, 0);
+      if (i === focus) fill(0, 0, 255);
+      else if (i === value) fill(128, 0, 128);
+      else if (max < i || i < min) fill(128, 128, 128);
+      rect(i * width / (elementsLength + 1) + 1, 1, width / elementsLength - 4, height - 2);
+    }
+  }
+  if (playing) {
+    if (frames % 60 / fps === 0) {
+      functions[functionsIndex]();
+      functionsIndex++;
+      functionsIndex %= functions.length;
+    }
+    frames++;
+  }
+}
+
+function findTarget() {
+  focus = (min + max) / 2;
+}
+
+function setBounds() {
+  if (focus === value) {
+    max = -1;
+    playing = false;
+  }
+  else if (focus < value) min = focus + 1;
+  else max = focus - 1;
+}
+
+function drawFocusValue() {
+  fill(0, 0, 255);
+  beginShape();
+  vertex(value * width / (elementsLength + 1) + 1, 2);
+  vertex(value * width / (elementsLength + 1) + 1, height - 1);
+  vertex((value + 1) * width / (elementsLength + 1) - 1, height - 1);
+  endShape(CLOSE);
+  fill(128, 0, 128);
+  beginShape();
+  vertex((value + 1) * width / (elementsLength + 1) - 1, 2);
+  vertex((value + 1) * width / (elementsLength + 1) - 1, height - 1);
+  vertex(value * width / (elementsLength + 1) + 1, 2);
+  endShape(CLOSE);
 }

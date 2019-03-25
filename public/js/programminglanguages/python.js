861a3f907ff18a83b3eedf01dd3f425c60811303
Dominic Wüst <42354311+DominicWuest@users.noreply.github.com>
2019-03-25 12:57:02 +0100

    Editor highlighting (#18)
    * Changed how code gets detected
    
    * finished highlighting for editor
    
    * Commit for sync
    
    * Finished editor Highlighting and Intendation
    
    * Fixed keywords in words getting highlighted
    
    * Fixed tabs not being detected
    
    * visible editor text no longer not moving
    
    * Removed function color in json file

--- public/js/programminglanguages/python.js
+++ public/js/programminglanguages/python.js
@@ -1,4 +1,4 @@
-var courseIndex; // Index of the course to be displayed
+let courseIndex; // Index of the course to be displayed
 
 if (document.cookie.split(';').filter((item) => item.trim().startsWith('courseIndex=')).length) {
   let value = "; " + document.cookie;
@@ -8,12 +8,12 @@ if (document.cookie.split(';').filter((item) => item.trim().startsWith('courseIn
   courseIndex = 0;
 }
 
-var subexerciseIndex = 0;
+let subexerciseIndex = 0;
 
 // output functions are configurable.  This one just appends some text
 // to a pre element.
 function outf(text) {
-    var myPre = document.getElementById('output');
+    let myPre = document.getElementById('output');
     myPre.innerHTML += text;
 }
 
@@ -28,17 +28,17 @@ function builtinRead(x) {
 // configure the output function
 // call Sk.importMainWithBody()
 function runit() {
-   var code = document.getElementById('code').value;
-   var myPre = document.getElementById('output');
+   let code = document.getElementById('textarea').value;
+   let myPre = document.getElementById('output');
    myPre.innerHTML = '';
    if (code === '') return;
    Sk.pre = 'output';
    Sk.configure({output:outf, read:builtinRead});
-   var myPromise = Sk.misceval.asyncToPromise(function() {
+   let myPromise = Sk.misceval.asyncToPromise(function() {
        return Sk.importMainWithBody('<stdin>', false, code, true);
    });
    myPromise.catch(function(err) { // Error in code
-     var span = document.createElement('SPAN');
+     let span = document.createElement('SPAN');
      span.classList.add('error');
      span.appendChild(document.createTextNode(err.toString()));
      myPre.appendChild(span);
@@ -51,31 +51,33 @@ function checkSolution(code) {
     document.getElementById('subExercise' + subexerciseIndex).classList.add('finishedSubexercise');
     document.getElementById('subExercise' + subexerciseIndex).classList.remove('incorrectSubexercise');
     subexerciseIndex++;
-    document.getElementById('code').value = pythonCourses['exercises'][courseIndex]['subexercises'][subexerciseIndex]['startingCode'];
+    document.getElementById('textarea').value = pythonCourses['exercises'][courseIndex]['subexercises'][subexerciseIndex]['startingCode'];
+    document.getElementById('textarea').oninput();
   } else {
     document.getElementById('subExercise' + subexerciseIndex).classList.add('incorrectSubexercise');
   }
 }
 
 function resetSubexercise() {
-  if (courseIndex === 0) document.getElementById('code').value = ''
-  else document.getElementById('code').value = pythonCourses['exercises'][courseIndex]['subexercises'][subexerciseIndex]['startingCode'];
+  if (courseIndex === 0) document.getElementById('textarea').value = ''
+  else document.getElementById('textarea').value = pythonCourses['exercises'][courseIndex]['subexercises'][subexerciseIndex]['startingCode'];
+  document.getElementById('textarea').oninput();
 }
 
 function displayExercise() {
   document.cookie = "courseIndex=" + courseIndex + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-  var exerciseTitle = document.getElementById('exerciseTitle');
+  let exerciseTitle = document.getElementById('exerciseTitle');
   exerciseTitle.innerHTML = '';
-  var title = document.createElement('H1');
+  let title = document.createElement('H1');
   title.appendChild(document.createTextNode(pythonCourses['exercises'][courseIndex]['title']));
   exerciseTitle.appendChild(title);
-  var textDiv = document.getElementById('exerciseText');
+  let textDiv = document.getElementById('exerciseText');
   textDiv.innerHTML = pythonCourses['exercises'][courseIndex]['description'];
   for (subexercise of pythonCourses['exercises'][courseIndex]['subexercises']) {
-    var subexerciseDiv = document.createElement('DIV');
+    let subexerciseDiv = document.createElement('DIV');
     subexerciseDiv.classList.add('subexercise');
     subexerciseDiv.id = 'subExercise' + subexercise['index'];
-    var subexerciseText = document.createElement('P');
+    let subexerciseText = document.createElement('P');
     subexerciseText.appendChild(document.createTextNode(subexercise['description']));
     subexerciseDiv.appendChild(subexerciseText);
     textDiv.appendChild(subexerciseDiv);
@@ -83,37 +85,66 @@ function displayExercise() {
   if (courseIndex !== 0) {
     document.getElementsByClassName('solutionButtonDiv')[0].style.visibility = 'visible';
     document.getElementsByName('solution')[0].style.visibility = 'visible';
-    document.getElementById('code').value = pythonCourses['exercises'][courseIndex]['subexercises'][0]['startingCode'];
+    document.getElementById('textarea').value = pythonCourses['exercises'][courseIndex]['subexercises'][0]['startingCode'];
   } else {
     document.getElementsByClassName('solutionButtonDiv')[0].style.visibility = 'hidden';
     document.getElementsByName('solution')[0].style.visibility = 'hidden';
   }
   makeDropdown();
+  document.getElementById('textarea').oninput();
 }
 
 function showSolution() {
-  document.getElementById('code').value = pythonCourses['exercises'][courseIndex]['subexercises'][subexerciseIndex]['solution'];
+  document.getElementById('textarea').value = pythonCourses['exercises'][courseIndex]['subexercises'][subexerciseIndex]['solution'];
+  document.getElementById('textarea').oninput();
 }
 
 function loaded() {
-  document.getElementById('code').addEventListener('keydown', function(event) { // Add event listener for tabs
-    if (event.keyCode === 13) {
-      var textArea = document.getElementById('code');
-      var cursorIndex = textArea.selectionStart - 1;
-      var code = textArea.value;
-      if (code.charAt(cursorIndex) === ':') {
-        var newCode = code.substring(0, cursorIndex + 1) + '\n\t' + code.substring(cursorIndex + 1, code.length);
-        textArea.value = newCode;
-        event.preventDefault();
+  document.getElementById('textarea').addEventListener('keydown', function(event) { // Add event listener for tabs
+    let caretPosition = event.target.selectionEnd;
+    if (event.keyCode === 13) { // If Enter key gets pressed
+      event.preventDefault();
+      let textArea = document.getElementById('textarea');
+      let cursorIndex = textArea.selectionStart - 1;
+      let code = textArea.value;
+      let colon = false;
+      if (code.charAt(cursorIndex) === ':') colon = true;
+      let codeArray = textArea.value.split('\n');
+      let index;
+      let length = cursorIndex;
+      for (let i = 0; i < codeArray.length; i++) {
+        let newLength = length - codeArray[i].length - 1;
+        if (newLength < 0) {
+          index = i;
+          length++;
+          break;
+        }
+        length = newLength;
       }
-    } else if (event.keyCode === 9) {
-      var textArea = document.getElementById('code');
-      var cursorIndex = textArea.selectionStart - 1;
-      var code = textArea.value;
-      var newCode = code.substring(0, cursorIndex + 1) + '\t' + code.substring(cursorIndex + 1, code.length);
+      let newLine = '\t'.repeat((' ' + codeArray[index]).substring(0, length + 1).split('\t').length + (colon ? 0 : -1)) + codeArray[index].substring(length);
+      codeArray[index] = codeArray[index].substring(0, length);
+      codeArray.splice(index + 1, 0, newLine);
+      textArea.value = codeArray.join('\n');
+      document.getElementById('textarea').oninput();
+      textArea.selectionEnd = caretPosition + (codeArray[index + 1].substring(0, length).split('\t').length);
+    } else if (event.keyCode === 9) { // If Tab key gets pressed
+      let textArea = document.getElementById('textarea');
+      let cursorIndex = textArea.selectionStart - 1;
+      let code = textArea.value;
+      let newCode = code.substring(0, cursorIndex + 1) + '\t' + code.substring(cursorIndex + 1, code.length);
       textArea.value = newCode;
       event.preventDefault();
+      document.getElementById('textarea').oninput();
+      textArea.selectionEnd = caretPosition + 1;
     }
+    document.getElementById('textarea').blur();
+    document.getElementById('textarea').focus();
+  });
+  document.getElementById('textarea').addEventListener('scroll', function(event) {
+    let element = document.getElementById('code');
+    console.log(event["target"]["scrollTop"])
+    element.scrollLeft = event["target"]["scrollLeft"];
+    element.scrollTop = event["target"]["scrollTop"];
   });
   makeDropdown();
   addHighlighting();
@@ -124,14 +155,14 @@ document.addEventListener('DOMContentLoaded', function(event) {
 })
 
 function makeDropdown() {
-  var list = document.getElementById('exercisesDropdownList');
+  let list = document.getElementById('exercisesDropdownList');
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
   for (course of pythonCourses['exercises']) {
     if (course['index'] !== courseIndex) {
-      var listElement = document.createElement('LI');
-      var textNode = document.createTextNode(course['title']);
+      let listElement = document.createElement('LI');
+      let textNode = document.createTextNode(course['title']);
       listElement.appendChild(textNode);
       listElement.id = course['index'];
       list.appendChild(listElement);
@@ -143,4 +174,9 @@ function makeDropdown() {
       });
     }
   }
+}
+
+function setTextareaDimensions() {
+  document.getElementById('textarea').style.width = document.getElementById('code').offsetWidth + 'px';
+  document.getElementById('textarea').style.height = document.getElementById('code').offsetHeight + 'px';
 }

5b2d76dda85f5b07a2d5d0cec42d3d301a984052
Dominic Wüst <42354311+DominicWuest@users.noreply.github.com>
2019-03-29 20:38:07 +0100

    Dominic comments (#22)
    * Added some comments and jsdocconf
    
    * Added comments to courses and codeHighlighting. Removed index.js
    
    * Added more comments to codeHighlighting.js
    
    * Added more comments
    
    * Added more comments
    
    * Finished adding comments to python.js.
    
    * Finished adding comments

--- public/js/algorithms/search/linear.js
+++ public/js/algorithms/search/linear.js
@@ -1,8 +1,19 @@
-let value, min, focus;
-let frames, fps = 3;
+// The index of the value to be found
+let value
+// The index upto which all elements are greyed out
+let min;
+// The index of the focused element
+let focus;
+// The amount of frames which have been displayed
+let frames;
+// The frames per second to be displayed
+let fps = 3;
+// A boolean indicating whether the animation is running or not
 let playing = false;
+// The amount of elements to be displayed
 let elementsLength;
 
+// Restart the animation completely
 function restartAnimation() {
   setup();
   let range = document.getElementById('range');
@@ -13,17 +24,24 @@ function restartAnimation() {
   frames = 0;
 }
 
+// Sets the size and position of the canvas
 function setup() {
   let canvasDiv = document.getElementById('animation');
   let width = canvasDiv.offsetWidth;
   let height = canvasDiv.offsetHeight;
+  // Creates canvas
   let canvas = createCanvas(width, height);
+  // Declares the parent div of the canvas
   canvas.parent('animation');
+  // Matches width of the range input to the width of the canvas
   document.getElementById('range').style.width = document.getElementById('animation').offsetWidth + "px";
 }
 
+// Gets called 60 times per second
 function draw() {
+  // Clear the canvas so that the background is transparent
   clear();
+  // Draw the values
   for (let i = 0; i <= elementsLength; i++) {
     if (i === focus && i === value) drawFocusValue();
     else {
@@ -34,28 +52,30 @@ function draw() {
       rect(i * width / (elementsLength + 1) + 1, 0, width / elementsLength - 4, height - 1);
     }
   }
-  if (playing) {
-    if (frames % (60 / fps) === 0) {
-      if (focus === value) {
-        console.log(width / elementsLength);
-        min = elementsLength;
-        playing = false;
-        focus--;
-      }
-      min++;
-      focus++;
+  // If 60 / fps frames have been desplayed and the animation is running
+  if (playing && frames++ % (60 / fps) === 0) {
+    // The value was found
+    if (focus === value) {
+      // Gray out all elements
+      min = elementsLength;
+      playing = false;
+      focus--;
     }
-    frames++;
+    min++;
+    focus++;
   }
 }
 
+// Draws the rectangle when the focused value equals the value to be found
 function drawFocusValue() {
+  // Draw the focused value
   fill(0, 0, 255);
   beginShape();
   vertex(value * width / (elementsLength + 1) + 1, 0);
   vertex(value * width / (elementsLength + 1) + 1, height);
   vertex((value + 1) * width / (elementsLength + 1) - 1, height);
   endShape(CLOSE);
+  // Draw the value to be found
   fill(128, 0, 128);
   beginShape();
   vertex((value + 1) * width / (elementsLength + 1) - 1, 0);

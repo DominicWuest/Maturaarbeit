6b35df7698d3daae4cc085d2be784d979f097204
DominicWuest <dominic.wuest@hotmail.com>
2019-03-30 01:33:49 +0100

    Fixed highlighting

--- public/js/codeHighlighting.js
+++ public/js/codeHighlighting.js
@@ -37,8 +37,8 @@ function addHighlighting() {
       // Characters to be ignored when checking to highlight syntax elements
       let ignoredCharacters = codeHighlighting[language]["ignoredCharacters"];
       // Colour all Syntax Elements
-      // Replaces a global search RegExp matching the syntax element with preceding and following characters to be ignored with the syntax element encapsulated in a span element and the ignored characters 
-      for (syntaxElement in codeHighlighting[language]["syntax"]) line = line.replace(new RegExp('([' + ignoredCharacters + ']*)' + syntaxElement + '([' + ignoredCharacters + ']*)', 'g'), '$1<span style="color: ' + codeHighlighting[language]["syntax"][syntaxElement] + '";>' + syntaxElement + '</span>$2');
+      // Replaces a global search RegExp matching the syntax element with preceding and following characters to be ignored with the syntax element encapsulated in a span element and the ignored characters
+      for (syntaxElement in codeHighlighting[language]["syntax"]) line = line.replace(new RegExp('([' + ignoredCharacters + ']+|^)' + syntaxElement + '([' + ignoredCharacters + ']+|$)', 'g'), '$1<span style="color: ' + codeHighlighting[language]["syntax"][syntaxElement] + '";>' + syntaxElement + '</span>$2');
       // Colour Comments
       line = line.split(codeHighlighting[language]["comments"]["character"]);
       // If the length of the splitted line is greater than one, there is a comment character included in the line

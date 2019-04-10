7d6c0ee8eabc9a10d361ac64598956eb6b93189a
DominicWolf <46967832+DominicWolf@users.noreply.github.com>
2019-04-10 13:10:08 +0200

    Update contact.js

--- public/js/contact.js
+++ public/js/contact.js
@@ -22,41 +22,7 @@ function checkInput(event) {
 
 	if (!inputStatus) event.preventDefault();
 }
-var i=0;
 var person = 1;
-function correct1() {
-	if (document.contactform.forename.value == "") {
-		document.getElementById("forename").placeholder = 'Bitte Feld korrekt ausfüllen!';
-		i+=1;
-	}
-}
-
-function correct2() {
-	if (document.contactform.surname.value == "") {
-		document.getElementById("surname").placeholder = 'Bitte Feld korrekt ausfüllen!';
-		i+=1;
-	}
-}
-
-function correct3() {
-	if (document.contactform.email.value == "") {
-		document.getElementById("email").placeholder = 'Bitte Feld korrekt ausfüllen!';
-		i+=1;
-	}
-}
-
-function correct4() {
-	if (document.contactform.message.value == "") {
-		document.getElementById("message").placeholder = 'Bitte Feld korrekt ausfüllen!';
-		i+=1;
-	}
-}
-
-function stop() {
-	if (i > 0) {
-		return false;
-	}
-}
 
 function checkwindow() {
 	if (window.innerWidth <= 1350) {

6e441e36c2a9b1c442d271a382f7bf5d687967a5
DominicWuest <dominic.wuest@hotmail.com>
2019-06-27 11:37:12 +0200

    Modified text lightly and added more code for the input-hasher

--- public/js/algorithms/hash/sha.js
+++ public/js/algorithms/hash/sha.js
@@ -1,4 +1,67 @@
+// The round Constants for the Keccak Algorithm
+let roundConstants = [
+	0x0000000000000001,
+	0x000000008000808B,
+	0x0000000000008082,
+	0x800000000000008B,
+	0x800000000000808A,
+	0x8000000000008089,
+	0x8000000080008000,
+	0x8000000000008003,
+	0x000000000000808B,
+	0x8000000000008002,
+	0x0000000080000001,
+	0x8000000000000080,
+	0x8000000080008081,
+	0x000000000000800A,
+	0x8000000000008009,
+	0x800000008000000A,
+	0x000000000000008A,
+	0x8000000080008081,
+	0x0000000000000088,
+	0x8000000000008080,
+	0x0000000080008009,
+	0x0000000080000001,
+	0x000000008000000A,
+	0x8000000080008008
+];
+
+// This function uses the SHA-3-Algorithm to hash the text input by the user
 function hash(message) {
+  // Creation of the byte-array
+  let bytes = [];
+  // Push the character codes of the input
+  for (let i = 0; i < message.length; i++) bytes.push(message.charCodeAt(i));
   let hashedMsg = 0;
   document.getElementById('hash').textContent = hashedMsg;
+}
+
+// Circular left rotation
+function leftRotate(num, shift) {
+  return (num << shift) | (num >>> (32 - shift));
+}
+
+function theta(a, x, y, z) {
+  let sumIndeces = [0, 1, 2, 3, 4];
+  return a[x][y][z] + sumIndeces.reduce(function(index, sum) => sum + a[x - 1][index][z]) + sumIndeces.reduce(unction(index, sum) => sum + a[x + 1][index][z - 1]);
+}
+
+function rho(a, x, y, z) {
+  
+}
+
+function pi(a, x, y) {
+
+}
+
+function chi(a, x) {
+
+}
+
+function iota(a) {
+
+}
+
+function rotationOffset(x, y) {
+
 }

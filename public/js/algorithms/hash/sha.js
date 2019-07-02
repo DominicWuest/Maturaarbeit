b22d004deea994063e31aa957492a418248b5db8
DominicWuest <dominic.wuest@hotmail.com>
2019-07-02 20:52:45 +0200

    Continued working on sha-3 algorithm

--- public/js/algorithms/hash/sha.js
+++ public/js/algorithms/hash/sha.js
@@ -1,3 +1,8 @@
+/*
+	Most of the variable names obey the ones from the FEDERAL INFORMATION PROCESSING STANDARDS (https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf)
+	A' is denoted as aA
+*/
+
 // The round Constants for the Keccak Algorithm
 let roundConstants = [
 	0x0000000000000001,
@@ -26,6 +31,8 @@ let roundConstants = [
 	0x8000000080008008
 ];
 
+let w;
+
 // This function uses the SHA-3-Algorithm to hash the text input by the user
 function hash(message) {
   // Creation of the byte-array
@@ -36,11 +43,8 @@ function hash(message) {
 
 function stringToState(string) {
   let bitArray = stringToBitArray(string);
-  let zeroArray = [];
-  for (let i = 0; i < 25 - ((bitArray.length + 2) % 25); i++) zeroArray.push(0);
-  bitArray = bitArray.concat([1], zeroArray, [1]);
   let A = [];
-  let w = bitArray.length / 25;
+  w = bitArray.length / 25;
   let yArr, zArr;
   for (let x = 0; x < 5; x++) {
     yArr = [];
@@ -59,7 +63,18 @@ function stringToBitArray(string) {
   for (byte of byteArray) {
     for (let i = 0; i < 8; i++) bitArray.push((byte >>> (7 - i)) & 0b1);
   }
+	bitArray = bitPadding(bitArray);
   return bitArray;
+}
+
+function bitPadding(arr) {
+	let len = arr.length;
+	let selectElement = document.getElementById('sha-value');
+	let neededLen = selectElement.options[selectElement.selectedIndex].value * 25;
+	arr.push(1);
+	while ((arr.length + 1) % neededLen !== 0) arr.push(0);
+	arr.push(1);
+	return arr;
 }
 
 // Circular left rotation
@@ -67,24 +82,69 @@ function leftRotate(num, shift) {
   return (num << shift) | (num >>> (32 - shift));
 }
 
-function theta(a, x, y, z) {
-  let sumIndeces = [0, 1, 2, 3, 4];
-  return a[x][y][z] ^ sumIndeces.reduce((index, sum) => sum ^ a[x - 1][index][z]) ^ sumIndeces.reduce((index, sum) => sum ^ a[x + 1][index][z - 1]);
+function theta(A) {
+	let C = [], CArr, num;
+	for (let x = 0; x < 5; x++) {
+		CArr = [];
+		for (let z = 0; z < w; z++) {
+			num = 0;
+			for (let y = 0; y < 5; y++) num ^= A[x][y][z];
+			CArr.push(num);
+		}
+		C.push(CArr);
+	}
+	let DArr = [], zArr;
+	for (let x = 0; x < 5; x++) {
+		zArr = [];
+		for (let z = 0; z < w; z++) zArr.push(C[(x - 1) % 5][z] ^ C[(x + 1) % 5][(z - 1) % w]);
+		DArr.push(zArr);
+	}
+	let aA = [], yArr;
+	for (let x = 0; x < 5; x++) {
+		yArr = [];
+		for (let y = 0; y < 5; y++) {
+			zArr = [];
+			for (let z = 0; z < w; z++) zArr.push(A[x][y][z] ^ D[x][z]);
+			yArr.push(zArr);
+		}
+		aA.push(yArr);
+	}
+	return aA;
 }
 
-function rho(a, x, y, z) {
+function rho(A, x, y, z) {
 
 }
 
-function pi(a, x, y) {
-
+function pi(A, x, y) {
+	let aA = [], yArr, zArr;
+	for (let x = 0; x < 5; x++) {
+		yArr = [];
+		for (let y = 0; y < 5; y++) {
+			zArr = [];
+			for (let z = 0; z < w; z++) zArr.push(A[(x + 3 * y) % 5][x][z]);
+			yArr.push(zArr);
+		}
+		aA.push(yArr);
+	}
+	return aA
 }
 
-function chi(a, x) {
-
+function chi(A, x) {
+	let aA = [], yArr, zArr;
+	for (let x = 0; x < 5; x++) {
+		yArr = [];
+		for (let y = 0; y < 5; y++) {
+			zArr = [];
+			for (let z = 0; z < w; z++) zArr.push(A[x][y][z] ^ ((A[(x + 1) % 5][y][z] ^ 1) & A[(x + 2) % 5][y][z]));
+			yArr.push(zArr);
+		}
+		aA.push(yArr);
+	}
+	return aA
 }
 
-function iota(a) {
+function iota(A) {
 
 }
 

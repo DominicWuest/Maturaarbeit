3f513d598c46cb60ed9e5fcf11b9e2add6ef4806
Daniel Isler <daniel.isler@students.ksba.ch>
2019-09-14 01:39:02 +0200

    added exercise to ASCII

---
+++ public/js/datacompression/entropy/utf.js
@@ -0,0 +1,108 @@
+function check1(message) {
+  document.getElementById('general').style.backgroundColor = "red";
+  if (message === '74') {
+    document.getElementById('ASCIIextable1').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable1').style.backgroundColor = "red";
+  }
+}
+
+function check2(message) {
+  if (message === '1110100') {
+    document.getElementById('ASCIIextable2').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable2').style.backgroundColor = "red";
+  }
+}
+
+function check3(message) {
+  if (message === '116') {
+    document.getElementById('ASCIIextable3').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable3').style.backgroundColor = "red";
+  }
+}
+
+function check4(message) {
+  if (message === 'k') {
+    document.getElementById('ASCIIextable4').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable4').style.backgroundColor = "red";
+  }
+}
+
+function check5(message) {
+  if (message === '6B') {
+    document.getElementById('ASCIIextable5').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable5').style.backgroundColor = "red";
+  }
+}
+
+function check6(message) {
+  if (message === '107') {
+    document.getElementById('ASCIIextable6').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable6').style.backgroundColor = "red";
+  }
+}
+
+function check7(message) {
+  if (message === '&') {
+    document.getElementById('ASCIIextable7').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable7').style.backgroundColor = "red";
+  }
+}
+
+function check8(message) {
+  if (message === '0100110') {
+    document.getElementById('ASCIIextable8').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable8').style.backgroundColor = "red";
+  }
+}
+
+function check9(message) {
+  if (message === '38') {
+    document.getElementById('ASCIIextable9').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable9').style.backgroundColor = "red";
+  }
+}
+
+function check10(message) {
+  if (message === 'o') {
+    document.getElementById('ASCIIextable10').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable10').style.backgroundColor = "red";
+  }
+}
+
+function check11(message) {
+  if (message === '6F') {
+    document.getElementById('ASCIIextable11').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable11').style.backgroundColor = "red";
+  }
+}
+
+function check12(message) {
+  if (message === '1101111') {
+    document.getElementById('ASCIIextable12').style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById('ASCIIextable12').style.backgroundColor = "red";
+  }
+}

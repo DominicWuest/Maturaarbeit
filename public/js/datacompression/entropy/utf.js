aee6c7d878502758d785a6546dfb4f0b31f3846f
Daniel Isler <daniel.isler@students.ksba.ch>
2019-09-14 03:18:28 +0200

    added full iso/iec 8859-1 table

--- public/js/datacompression/entropy/utf.js
+++ public/js/datacompression/entropy/utf.js
@@ -1,10 +1,10 @@
 // Define solution input values
-let inputs = ['74', '1110100', '116', 'k', '6B', '107', '&', '0100110', '38', 'o', '6F', '1101111']
+let inputsASCII = ['74', '1110100', '116', 'k', '6B', '107', '&', '0100110', '38', 'o', '6F', '1101111']
 
 // Check whether the given input matches the solution
-function check(message, index) {
+function checkASCII(message, index) {
   // If the input matches the solution display a green background
-  if (message === inputs[index]) {
+  if (message === inputsASCII[index]) {
     document.getElementById('ASCIIextable' + index).style.backgroundColor = "green";
   }
   // If the input is wrong display a red background

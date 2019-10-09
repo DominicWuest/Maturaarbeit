60755a0951ab4b0691a95de9291b9c6740d7c069
Daniel Isler <daniel.isler@students.ksba.ch>
2019-10-09 23:47:00 +0200

    played a bit with jquery and freshed up design

---
+++ public/js/datacompression/entropy/jquerydesigns.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  $('textarea').focus(function() {
+    $(this).css("background-color", "#DDD")
+  });
+
+  $('textarea').blur(function() {
+    $(this).css("background-color", "#FFF")
+  });
+
+  $('input[type=text]').focus(function() {
+    $(this).css("background-color", "#DDD")
+  });
+
+  $('input[type=text]').blur(function() {
+    $(this).css("background-color", "#FFF")
+  });
+
+  $('.numbers').scroll(function(){
+    $('.numbers').scrollTop($(this).scrollTop());
+  })
+
+});

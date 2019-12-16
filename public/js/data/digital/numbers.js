65a71053f33af3b8e4f1fb7baf57628bd7860370
Daniel Isler <daniel.isler@students.ksba.ch>
2019-12-16 17:51:14 +0100

    added twos complement exercise

--- public/js/data/digital/numbers.js
+++ public/js/data/digital/numbers.js
@@ -6,6 +6,9 @@ function displayExercise(n) {
   for (let i = 0; i < n; i++) {
     displayConvert(i);
   }
+  for (let i = 0; i < n; i++) {
+    displayComplement(i);
+  }
 }
 
 // Displays the format conversion exercise
@@ -22,6 +25,30 @@ function displayConvert(index) {
   buttonList[randSlot + 3 * index].style.visibility = 'hidden';
 }
 
+// Displays the format complementexercise
+function displayComplement(index) {
+  solution.push([Math.floor((Math.random() - 0.5) * Math.random() * 150 * (index + 1))]);
+  let a = solution[index + 4][0];
+  if (a < 0) {
+    var bin = (solution[index + 4][0] >>> 0).toString(2).substring(17);
+  }
+  else {
+    var bin = (a >>> 0).toString(2);
+    var zeros = 15 - bin.length;
+    for (let i = 0; i < zeros; i++) bin = '0'.concat(bin);
+  }
+  solution[index + 4].push((Math.abs(a) >>> 0).toString(2), bin);
+  randSlot = Math.floor(Math.random() * 3);
+  var fieldList = document.getElementsByClassName("complementexercise");
+  var inputList = document.getElementsByClassName("inputComplement");
+  var buttonList = document.getElementsByClassName("complementSolution");
+  inputList[randSlot + 3 * index].value = solution[index + 4][randSlot];
+  inputList[randSlot + 3 * index].disabled = true;
+  fieldList[randSlot + 3 * index].style.backgroundColor = "blue";
+  buttonList[randSlot + 3 * index].style.visibility = 'hidden';
+}
+
+// Checks the input value
 function checkConvert(message, index) {
   if (message == solution[Math.floor(index / 3)][index % 3]) {
     document.getElementsByClassName("convertexercise")[index].style.backgroundColor = "green";
@@ -31,11 +58,29 @@ function checkConvert(message, index) {
   }
 }
 
+// Checks the input value
+function checkComplement(message, index) {
+  if (message == solution[Math.floor(index / 3) + 4][index % 3]) {
+    document.getElementsByClassName("complementexercise")[index].style.backgroundColor = "green";
+  }
+  else {
+    document.getElementsByClassName("complementexercise")[index].style.backgroundColor = "red";
+  }
+}
+
+// Displays the solution to the user
 function solutionConvert(index) {
   document.getElementsByClassName("inputConvert")[index].value = solution[Math.floor(index / 3)][index % 3];
   document.getElementsByClassName("convertexercise")[index].style.backgroundColor = "green";
 }
 
+// Displays the solution to the user
+function solutionComplement(index) {
+  document.getElementsByClassName("inputComplement")[index].value = solution[Math.floor(index / 3) + 4][index % 3];
+  document.getElementsByClassName("complementexercise")[index].style.backgroundColor = "green";
+}
+
+// Displays new exercises
 function resetConvert() {
   for (let i = 0; i < 12; i++) {
     document.getElementsByClassName("inputConvert")[i].value = '';
@@ -43,6 +88,18 @@ function resetConvert() {
     document.getElementsByClassName("convertexercise")[i].style.backgroundColor = "white";
     document.getElementsByClassName("convertSolution")[i].style.visibility = 'visible';
   }
-  solution = [];
+  for (let i = 0; i < 4; i++) solution.shift();
   for (let i = 0; i < 4; i++) displayConvert(i);
+}
+
+// Displays new exercises
+function resetComplement() {
+  for (let i = 0; i < 12; i++) {
+    document.getElementsByClassName("inputComplement")[i].value = '';
+    document.getElementsByClassName("inputComplement")[i].disabled = false;
+    document.getElementsByClassName("complementexercise")[i].style.backgroundColor = "white";
+    document.getElementsByClassName("complementSolution")[i].style.visibility = 'visible';
+  }
+  for (let i = 0; i < 4; i++) solution.pop();
+  for (let i = 0; i < 4; i++) displayComplement(i);
 }

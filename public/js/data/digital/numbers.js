dc743af1e9a428463bcce06ea39f0b9aff4eb935
Daniel Isler <daniel.isler@students.ksba.ch>
2019-12-21 21:22:26 +0100

    Datasites (#95)
    * tried different styles with the footer
    
    * fixed last validation issues
    
    * improved styling for mobiles
    
    * added better styling for small devices
    
    * downloaded images und fixed styling
    
    * finished responsive design
    
    * played a bit with jquery and freshed up design
    
    * fixed line number shift
    
    * added more space for coding
    
    * fixed line numbers height
    
    * removed unnecessary function
    
    * freshed up styling or safari browser
    
    * hide the solution button where the input is given
    
    * removed the jqueryfun and built it into the css
    
    * removed the jqueryfun and built it into the css
    
    * styled it a bit better
    
    * gave the code a better style
    
    * removed spellcheck and unnecessary attributes
    
    * corrected all sites on spelling
    
    * fixed issue with user spamming start button
    
    * removed unnecessary waiting time
    
    * fixed indent and removed java in text
    
    * added basic files and included new style in old navigation pages
    
    * added navigation pages to all algorithm subcategories
    
    * fixed styling issues on the phone
    
    * added navigation pages for the protocols charpters
    
    * added the new style to all navigation pages in a common responsive stylesheet and removed the old ones
    
    * update 21.10.2019
    
    * added description to last two navigation pages
    
    * added cross-trough for not existing courses
    
    * added first two new styled navigationpages and changed pictures to license-free ones
    
    * changed navigationpages code everywhere it was necessary
    
    * fixed style issue
    
    * finished code restyling for all navigation pages where it was senceful
    
    * update 31.10.2019
    
    * styled journal a bit and updated newest courses
    
    * fixed validation error
    
    * fixed indent in example code
    
    * added new highlighting version
    
    * fixed link to all courses
    
    * finished editing highlighting
    
    * fixed unwanted change
    
    * fixed indent to make code look better
    
    * fixed indent to make code look better
    
    * added basic file
    
    * update
    
    * added css for style of formulas
    
    * added example for encoding
    
    * added exercise generator
    
    * created starting interval
    
    * kept working on the program
    
    * finished programm and added decoding part
    
    * fixed small mistakes
    
    * fixed older mistakes
    
    * added new courses and rearranged the datacompression charpter to build it more senseful
    
    * fixed navigationpages css tu unify them better and addad data navigationpage
    
    * removed unwanted file
    
    * removed unwanted file
    
    * removed unwanted file
    
    * added new sites and resolved imgs folder
    
    * added basic files with exercise for conversion
    
    * added twos complement exercise
    
    * added description of convert and negative numbers parts
    
    * started with floating point numbers
    
    * finished floatingpoint numbers part
    
    * freshed up code with new knowhow
    
    * built all exercises with ejs and fixed code in numbers.js
    
    * added signs navigation page

---
+++ public/js/data/digital/numbers.js
@@ -0,0 +1,207 @@
+// Stores all the solutions to the exercises
+let solution = [];
+
+// Displays all the exercises
+function displayExercise(n) {
+  for (let i = 0; i < n; i++) {
+    displayConvert(i, true);
+  }
+  for (let i = 0; i < n; i++) {
+    displayComplement(i, true);
+  }
+  for (let i = 0; i < n; i++) {
+    displayFloat(i, true);
+  }
+}
+
+// Displays the format conversion exercise
+function displayConvert(index, first) {
+  console.log(solution);
+  if (first) solution.push([Math.floor(Math.random() * 100 * (index + 1))]);
+  else solution.splice(index, 1, [Math.floor(Math.random() * 100 * (index + 1))]);
+  solution[index].push((solution[index][0] >>> 0).toString(16).toUpperCase(), (solution[index][0] >>> 0).toString(2));
+  randSlot = Math.floor(Math.random() * 3);
+  var fieldList = document.getElementsByClassName("convertexercise");
+  var inputList = document.getElementsByClassName("inputConvert");
+  var buttonList = document.getElementsByClassName("convertSolution");
+  inputList[randSlot + 3 * index].value = solution[index][randSlot];
+  inputList[randSlot + 3 * index].disabled = true;
+  fieldList[randSlot + 3 * index].style.backgroundColor = "blue";
+  buttonList[randSlot + 3 * index].style.visibility = 'hidden';
+}
+
+// Displays the format complementexercise
+function displayComplement(index, first) {
+  if (first) solution.push([Math.floor((Math.random() - 0.5) * Math.random() * 150 * (index + 1))]);
+  else solution.splice(index + 4, 1, [Math.floor((Math.random() - 0.5) * Math.random() * 150 * (index + 1))]);
+  let a = solution[index + 4][0];
+  if (a < 0) {
+    var bin = (solution[index + 4][0] >>> 0).toString(2).substring(17);
+  }
+  else {
+    var bin = (a >>> 0).toString(2);
+    var zeros = 15 - bin.length;
+    for (let i = 0; i < zeros; i++) bin = '0'.concat(bin);
+  }
+  solution[index + 4].push((Math.abs(a) >>> 0).toString(2), bin);
+  var fieldList = document.getElementsByClassName("complementexercise");
+  var inputList = document.getElementsByClassName("inputComplement");
+  var buttonList = document.getElementsByClassName("complementSolution");
+  if (Math.random() < 0.5) {
+    inputList[0 + 3 * index].value = solution[index + 4][0];
+    inputList[0 + 3 * index].disabled = true;
+    fieldList[0 + 3 * index].style.backgroundColor = "blue";
+    buttonList[0 + 3 * index].style.visibility = 'hidden';
+  }
+  else {
+    inputList[2 + 3 * index].value = solution[index + 4][2];
+    inputList[2 + 3 * index].disabled = true;
+    fieldList[2 + 3 * index].style.backgroundColor = "blue";
+    buttonList[2 + 3 * index].style.visibility = 'hidden';
+  }
+}
+
+// Displays the format floatexercise
+function displayFloat(index, first) {
+  if (first) solution.push([(Math.random() - 0.5) * Math.random() * 150 * (((index + 1) / 3) ** 13)]);
+  else solution.splice(index + 8, 1, [(Math.random() - 0.5) * Math.random() * 150 * (((index + 1) / 3) ** 13)])
+  solution[index + 8].push(Math.sign(Math.sign(solution[index + 8][0]) + 1) - Math.sign(solution[index + 8][0]), exponent(solution[index + 8][0]), mantissa(Math.abs(solution[index + 8][0])));
+  var fieldList = document.getElementsByClassName("floatexercise");
+  var inputList = document.getElementsByClassName("inputFloat");
+  var buttonList = document.getElementsByClassName("floatSolution");
+  if (Math.random() < 0.5) {
+    inputList[0 + 4 * index].value = solution[index + 8][0];
+    inputList[0 + 4 * index].disabled = true;
+    fieldList[0 + 4 * index].style.backgroundColor = "blue";
+    buttonList[0 + 4 * index].style.visibility = 'hidden';
+  }
+  else {
+    inputList[1 + 4 * index].value = solution[index + 8][1];
+    inputList[1 + 4 * index].disabled = true;
+    fieldList[1 + 4 * index].style.backgroundColor = "blue";
+    buttonList[1 + 4 * index].style.visibility = 'hidden';
+    inputList[2 + 4 * index].value = solution[index + 8][2];
+    inputList[2 + 4 * index].disabled = true;
+    fieldList[2 + 4 * index].style.backgroundColor = "blue";
+    buttonList[2 + 4 * index].style.visibility = 'hidden';
+    inputList[3 + 4 * index].value = solution[index + 8][3];
+    inputList[3 + 4 * index].disabled = true;
+    fieldList[3 + 4 * index].style.backgroundColor = "blue";
+    buttonList[3 + 4 * index].style.visibility = 'hidden';
+  }
+}
+
+// Calculates the fp exponent of a number
+function exponent(n) {
+  var exp = 127;
+  while (Math.abs(n) > 2) {
+    exp++;
+    n = n / 2;
+  }
+  while (Math.abs(n) < 1 && n != 0) {
+    exp--;
+    n = n * 2;
+  }
+  if (n == 0) exp = 0;
+  exp = (exp >>> 0).toString(2);
+  while (exp.length < 8) exp = '0'.concat(exp);
+  return exp;
+}
+
+// Caltulates the mantissa of a number
+function mantissa(n) {
+  while (n > 2) n = n / 2;
+  while (n < 1 && n != 0) n = n * 2;
+  if (n > 1) n = n - 1;
+  var mant = [];
+  for (let i = 1; i < 15; i++) {
+    if (n > 2 ** (-i)) {
+      n = n - 2 ** (-i);
+      mant.push(1);
+    }
+    else mant.push(0);
+  }
+  mant = mant.join('');
+  return mant;
+}
+
+// Checks the input value
+function checkConvert(message, index) {
+  if (message == solution[Math.floor(index / 3)][index % 3]) {
+    document.getElementsByClassName("convertexercise")[index].style.backgroundColor = "green";
+  }
+  else {
+    document.getElementsByClassName("convertexercise")[index].style.backgroundColor = "red";
+  }
+}
+
+// Checks the input value
+function checkComplement(message, index) {
+  if (message == solution[Math.floor(index / 3) + 4][index % 3]) {
+    document.getElementsByClassName("complementexercise")[index].style.backgroundColor = "green";
+  }
+  else {
+    document.getElementsByClassName("complementexercise")[index].style.backgroundColor = "red";
+  }
+}
+
+// Checks the input value
+function checkFloat(message, index) {
+  if (message == solution[Math.floor(index / 4) + 8][index % 4]) {
+    document.getElementsByClassName("floatexercise")[index].style.backgroundColor = "green";
+  }
+  else {
+    document.getElementsByClassName("floatexercise")[index].style.backgroundColor = "red";
+  }
+}
+
+// Displays the solution to the user
+function solutionConvert(index) {
+  document.getElementsByClassName("inputConvert")[index].value = solution[Math.floor(index / 3)][index % 3];
+  document.getElementsByClassName("convertexercise")[index].style.backgroundColor = "green";
+}
+
+// Displays the solution to the user
+function solutionComplement(index) {
+  document.getElementsByClassName("inputComplement")[index].value = solution[Math.floor(index / 3) + 4][index % 3];
+  document.getElementsByClassName("complementexercise")[index].style.backgroundColor = "green";
+}
+
+// Displays the solution to the user
+function solutionFloat(index) {
+  document.getElementsByClassName("inputFloat")[index].value = solution[Math.floor(index / 4) + 8][index % 4];
+  document.getElementsByClassName("floatexercise")[index].style.backgroundColor = "green";
+}
+
+// Displays new exercises
+function resetConvert() {
+  for (let i = 0; i < 12; i++) {
+    document.getElementsByClassName("inputConvert")[i].value = '';
+    document.getElementsByClassName("inputConvert")[i].disabled = false;
+    document.getElementsByClassName("convertexercise")[i].style.backgroundColor = "white";
+    document.getElementsByClassName("convertSolution")[i].style.visibility = 'visible';
+  }
+  for (let i = 0; i < 4; i++) displayConvert(i, false);
+}
+
+// Displays new exercises
+function resetComplement() {
+  for (let i = 0; i < 12; i++) {
+    document.getElementsByClassName("inputComplement")[i].value = '';
+    document.getElementsByClassName("inputComplement")[i].disabled = false;
+    document.getElementsByClassName("complementexercise")[i].style.backgroundColor = "white";
+    document.getElementsByClassName("complementSolution")[i].style.visibility = 'visible';
+  }
+  for (let i = 0; i < 4; i++) displayComplement(i);
+}
+
+// Displays new exercises
+function resetFloat() {
+  for (let i = 0; i < 16; i++) {
+    document.getElementsByClassName("inputFloat")[i].value = '';
+    document.getElementsByClassName("inputFloat")[i].disabled = false;
+    document.getElementsByClassName("floatexercise")[i].style.backgroundColor = "white";
+    document.getElementsByClassName("floatSolution")[i].style.visibility = 'visible';
+  }
+  for (let i = 0; i < 4; i++) displayFloat(i);
+}
